3b2a74a8f31788f8f927e37aa364e3b3a96196ab
box_chang <box_chang@mail.eagleskytech.com>
2019-07-22 16:31:34 +0800

    fix

--- ping_server.py
+++ ping_server.py
@@ -6,27 +6,47 @@ from lineNotifyMessage import lineNotifyMessage
 
 
 def main():
+    bpm_server = check_server_factory('BPM Server', '10.77.9.103', '8086')
+    bpm_server.check()
+    erp_server = check_server_factory('BPM Server', '10.77.9.101', '80')
+    erp_server.check()
 
-    host = '10.77.9.103'
-    port = 8086
 
-    check_socket(host, port)
+def send_line_message(server, host, port):
+    # callBoxLine("BPM Server can not be used now!!")
+
+    # 修改為你要傳送的訊息內容
+    message = server + '(' + host + ') Port ' + port + ' 無法Ping通!'
+    # 修改為你的權杖內容
+    token = 'UslqqsBVGwcn0RJcI9R9vyx5TeWX66wYP6AlZtnHU9v'
+
+    lineNotifyMessage(token, message)
+
+
+class check_server_factory():
+
+    def __init__(self, server, host, port_list):
+        self.server = server
+        self.host = host
+        self.port_list = port_list
+
+    def check(self):
+        port_list = self.port_list.split(':')
+        for port in port_list:
+            if not check_socket(self.host, int(port)):
+                send_line_message(self.server, self.host, port)
+
 
 def check_socket(host, port):
     with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
         if sock.connect_ex((host, port)) == 0:
             print
             "Port is open"
+            return True
         else:
             print
             "Port is not open"
-            # callBoxLine("BPM Server can not be used now!!")
+            return False
 
-            # 修改為你要傳送的訊息內容
-            message = 'BPM Server 10.77.9.103 Port 8086 無法Ping通!'
-            # 修改為你的權杖內容
-            token = 'UslqqsBVGwcn0RJcI9R9vyx5TeWX66wYP6AlZtnHU9v'
-
-            lineNotifyMessage(token, message)
 if __name__ == '__main__':
     main()

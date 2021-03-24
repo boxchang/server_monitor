28831e7de421c8ccdf4848107bf76c903947c2df
box_chang <box_chang@mail.eagleskytech.com>
2021-03-24 16:30:59 +0800

    source code

--- check_db_backup2.py
+++ check_db_backup2.py
@@ -3,7 +3,11 @@ from smb.SMBConnection import SMBConnection
 import datetime
 from lineNotifyMessage import lineNotifyMessage
 
-def Check_Mapping_Network_Driver_File(word):
+def Check_Mapping_Network_Driver_File():
+    bpm_result = False
+    tiptop_result = False
+    msg = ""
+
     ip = '10.77.9.50'
 
     name = socket.gethostbyaddr(ip)
@@ -30,12 +34,29 @@ def Check_Mapping_Network_Driver_File(word):
         print('type', s.type)
         print('')
         print('### FileList ###')
-        try:
-            for f in conn.listPath(s.name, '/Prod_db_backup'):
 
-                if f.filename.find(word) > 0:
-                    print(f.filename)
-                    return True
+        try:
+            yesterday = datetime.date.today() + datetime.timedelta(-1)
+
+            if s.name == "BPM_BACKUP":
+                for f in conn.listPath(s.name, '/Prod_db_backup'):
+                    word = yesterday.strftime("%Y_%m_%d")
+                    if f.filename.find(word) > 0:
+                        print(f.filename)
+                        bpm_result = True
+
+                if not bpm_result:
+                    msg += "BPM DB Backup少檔案" + word + "\n"
+
+            if s.name == "TIPTOP_BACKUP":
+                for f in conn.listPath(s.name, '/Prod_db_backup'):
+                    word = yesterday.strftime("%Y%m%d")
+                    if f.filename == word:
+                        print(f.filename)
+                        tiptop_result = True
+
+                if not tiptop_result:
+                    msg += "TIPTOP DB Backup少檔案" + word + "\n"
 
         except:
             print('### can not access the resource')
@@ -43,12 +64,10 @@ def Check_Mapping_Network_Driver_File(word):
         print('')
 
     conn.close()
-    return False
+    return bpm_result&tiptop_result, msg
 
-yesterday = datetime.date.today() + datetime.timedelta(-1)
-yesterday = yesterday.strftime("%Y_%m_%d")
+result, message = Check_Mapping_Network_Driver_File()
 
-if not Check_Mapping_Network_Driver_File(yesterday):
+if not result:
     token = 'kB5Gh2KDGLi8Te6nGgXYxXh5qoIlVLjepkQbi5sEVIS'
-    message = "BPM DB Backup少檔案"+yesterday
     lineNotifyMessage(token, message)

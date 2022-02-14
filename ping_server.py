b892dff1dd21de322d0f329397be1fdc80a86b6c
box_chang <box_chang@mail.eagleskytech.com>
2022-02-14 16:52:04 +0800

    source code

--- ping_server.py
+++ ping_server.py
@@ -8,11 +8,8 @@ from lineNotifyMessage import lineNotifyMessage
 
 def main():
     erp_server1 = check_server_factory('ERP Server', '10.77.9.1', '80', 'ERP八○')
-    erp_server2 = check_server_factory('ERP Server', '210.4.114.243', '18081', 'ERP(外)一八○八一')
     bpm_server1 = check_server_factory('BPM Server', '10.77.9.3', '8086', 'BPM八○八六')
-    bpm_server2 = check_server_factory('BPM Server', '210.4.114.243', '18083', 'BPM(外)一八○八三')
     crt_server1 = check_server_factory('Crystal Report Server', '10.77.9.2', '80', 'CRT八○')
-    crt_server2 = check_server_factory('Crystal Report Server', '210.4.114.243', '80', 'CRT(外)八○')
 
     print("Start Monitor")
     while(True):
@@ -27,15 +24,6 @@ def main():
         if erp_server1.getCount() > 3:
             send_line_message(erp_server1.server, erp_server1.host, erp_server1, erp_server1.msg)
 
-        result = erp_server2.check()
-        if not result:
-            erp_server2.count()
-        else:
-            erp_server2.reset_count()
-
-        if erp_server2.getCount() > 3:
-            send_line_message(erp_server2.msg)
-
 
         # BPM
         result = bpm_server1.check()
@@ -46,15 +34,6 @@ def main():
 
         if bpm_server1.getCount() > 3:
             send_line_message(bpm_server1.msg)
-
-        result = bpm_server2.check()
-        if not result:
-            bpm_server2.count()
-        else:
-            bpm_server2.reset_count()
-
-        if bpm_server2.getCount() > 3:
-            send_line_message(bpm_server2.msg)
 
 
         # Crystal Report
@@ -67,14 +46,6 @@ def main():
         if crt_server1.getCount() > 3:
             send_line_message(crt_server1.msg)
 
-        result = crt_server2.check()
-        if not result:
-            crt_server2.count()
-        else:
-            crt_server2.reset_count()
-
-        if crt_server2.getCount() > 3:
-            send_line_message(crt_server2.msg)
 
         print("Stop Check")
         time.sleep(60)
